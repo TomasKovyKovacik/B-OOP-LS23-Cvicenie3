6603ae934bf2fa0547234502bf25aaefe2e2351b
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-01 14:19:04 +0100

    prvy commit - auto

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,10 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.auto.Auto;
+
 public class Main {
     public static void main(String[] args) {
+        Auto volvo = new Auto();
 
     }
 }
---
+++ src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
@@ -0,0 +1,12 @@
+package sk.stuba.fei.uim.oop.auto;
+
+public class Auto {
+    int pocetKolies;
+    int pocetDveri;
+    String nazov;
+
+    public int jedna;
+    protected int dva;
+    int tri;
+    private int styri;
+}
