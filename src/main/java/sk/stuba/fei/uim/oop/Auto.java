89ac4893f2dc15c73ff1f18ec5a5ab3ef7d477d7
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-01 14:46:45 +0100

    konstruktory

--- src/main/java/sk/stuba/fei/uim/oop/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/Auto.java
@@ -1,12 +1,23 @@
-package sk.stuba.fei.uim.oop.auto;
+package sk.stuba.fei.uim.oop;
 
 public class Auto {
+    public static final double PALIVO_NA_KM = 0.5;
     private double stavNadrze;
     private double kapacitaNadrze;
     private boolean ojazdene;
 
+    public Auto() {
+        this(50, 100);
+    }
+
+    public Auto(double stavNadrze, double kapacitaNadrze){
+        this.stavNadrze = stavNadrze;
+        this.kapacitaNadrze = kapacitaNadrze;
+        this.ojazdene = true;
+    }
+
     public void jazdi(double vzdialenostVKm) {
-        stavNadrze = stavNadrze - vzdialenostVKm * 0.5;
+        stavNadrze = stavNadrze - vzdialenostVKm * PALIVO_NA_KM;
         if (stavNadrze < 0) {
             stavNadrze = 0;
             System.out.println("Mame prazdnu nadrz");
--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,12 +1,8 @@
 package sk.stuba.fei.uim.oop;
 
-import sk.stuba.fei.uim.oop.auto.Auto;
-
 public class Main {
     public static void main(String[] args) {
-        Auto a = new Auto();
-        a.setKapacitaNadrze(100);
-        a.setStavNadrze(100);
+        Auto a = new Auto(100, 100);
 
         System.out.println(a.getStavNadrze());
         a.jazdi(100);
