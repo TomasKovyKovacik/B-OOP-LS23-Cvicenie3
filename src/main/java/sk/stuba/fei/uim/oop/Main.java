772c37546490228a0db281dde88736cfd68b20fe
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-01 14:28:38 +0100

    getre a settre

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -4,7 +4,12 @@ import sk.stuba.fei.uim.oop.auto.Auto;
 
 public class Main {
     public static void main(String[] args) {
-        Auto volvo = new Auto();
+        Auto a = new Auto();
+        a.setKapacitaNadrze(100);
+        a.setStavNadrze(100);
 
+        System.out.println(a.getStavNadrze());
+        a.jazdi(100);
+        System.out.println(a.getStavNadrze());
     }
 }
--- src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
@@ -1,12 +1,39 @@
 package sk.stuba.fei.uim.oop.auto;
 
 public class Auto {
-    int pocetKolies;
-    int pocetDveri;
-    String nazov;
+    private double stavNadrze;
+    private double kapacitaNadrze;
+    private boolean ojazdene;
 
-    public int jedna;
-    protected int dva;
-    int tri;
-    private int styri;
+    public void jazdi(double vzdialenostVKm) {
+        stavNadrze = stavNadrze - vzdialenostVKm * 0.5;
+        if (stavNadrze < 0) {
+            stavNadrze = 0;
+            System.out.println("Mame prazdnu nadrz");
+        }
+    }
+
+    public double getStavNadrze() {
+        return stavNadrze;
+    }
+
+    public void setStavNadrze(double stavNadrze) {
+        this.stavNadrze = stavNadrze;
+    }
+
+    public double getKapacitaNadrze() {
+        return kapacitaNadrze;
+    }
+
+    public void setKapacitaNadrze(double kapacitaNadrze) {
+        this.kapacitaNadrze = kapacitaNadrze;
+    }
+
+    public boolean isOjazdene() {
+        return ojazdene;
+    }
+
+    public void setOjazdene(boolean ojazdene) {
+        this.ojazdene = ojazdene;
+    }
 }
