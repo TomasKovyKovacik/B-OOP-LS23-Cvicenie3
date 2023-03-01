5cc16edd4f1c7c9270affbb10a6bce834651140b
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-01 14:58:16 +0100

    z klavesnice a keyboard input

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,11 +1,14 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.utility.KeyboardInput;
+import sk.stuba.fei.uim.oop.utility.ZKlavesnice;
+
 public class Main {
     public static void main(String[] args) {
-        Auto a = new Auto(100, 100);
-
-        System.out.println(a.getStavNadrze());
-        a.jazdi(100);
-        System.out.println(a.getStavNadrze());
+        String meno1 = ZKlavesnice.readString("Zadaj meno");
+        System.out.println("Nacitane Meno : " + meno1);
+        System.out.println("--------------------------------");
+        Double cislo = KeyboardInput.readDouble("Zadaj cislo", 1);
+        System.out.println("Nacitane cislo " + cislo);
     }
 }
